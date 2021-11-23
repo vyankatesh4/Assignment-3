0b94cb9b071da62c1d559cfa9fb24b90dc2bed07
vyankatesh dube <93048498+vyankatesh4@users.noreply.github.com>
2021-11-23 16:51:05 +0530

    Add files via upload

---
+++ A3Q1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+sample_list = [8, 2, 3, 0, 7]
+print(type(sample_list))
+
+
+# In[4]:
+
+
+import math
+print(math.fsum(sample_list))
+
+
+# In[ ]:
+
+
+
+

52402c94f0168c321891e7c61fa350b1b51a32f3
vyankatesh dube <93048498+vyankatesh4@users.noreply.github.com>
2021-11-23 17:04:30 +0530

    Add files via upload

---
+++ A3Q1_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+sample_list = [8, 2, 3, 0, 7]
+def sum(sample_list):
+    total = 0
+    for i in sample_list:
+        total += i
+    return total
+print(sum(sample_list))
+
+
+# In[ ]:
+
+
+
+
